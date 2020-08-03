64596ea0b282b47d1d68fbe174c3d8fcd3ebc438
asobisarch <asobisarch@gmail.com>
2020-08-03 02:00:10 +0000

    コメント削除

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -34,7 +34,6 @@ class RoomChannel < ApplicationCable::Channel
     ng_word2 = ng_word.split(',').map { |m| m.delete('[]"\\\\')}
     ng_word_params = ng_word2.map {|m| m.gsub(' ', "")}
 
-    # ng_word_params = ["ナイフ","死ね","しね", "4ね","氏ね","cね","ｃね","殺","市ね", "sine", "fack","fuck", "くそやろ" ,"クソ野郎","56す", "住所","刺す", "殺す", "殺してやる", "変態","野獣先輩", "キチガイ", "セクハラ", "ぶっ殺す", "ちんこ", "キス", "うんち", "うんこ", "ゴミ", "おっぱい", "まんこ", "性器", "せっくす", "SEX", "サル", "陰キャ", "カス", "かす", "ぶす", "ブス", "ぶ/す", "ぶっす", "ぶさいく", "ブサイク", "ぶっすう", "あの世", "血", "グロ", "ばばぁ","じじい", "婆", "爺","老害", "障害" ,"ちび", "チビ", "雑魚", "あほ", "アホ", "バカ", "馬鹿", "bs"]
     ip = self.connection.ip_addr
     # puts "==========================" + current_user.name
     # user_signed_in? = self.connection.signed_in
